6a775ce08b3225cbfaaf18998eda938cde6164fa
illu92 <pablomorpal@gmail.com>
2018-01-03 11:55:09 +0100

    Ended

--- class1_3.py
+++ class1_3.py
@@ -1,1 +1,9 @@
 # Class 1.3
+
+# type() indicates the type of the variable between ()
+type(4)             #int
+type(3.14)          #float
+type('Hola mundo')  #string
+
+student = "Pablo"
+type(student)       #string
