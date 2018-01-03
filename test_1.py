942a1b4c8769899b241b1154651abf3ebc73fb90
illu92 <pablomorpal@gmail.com>
2018-01-03 01:47:03 +0100

    Primeras pruebas con las lecciones de edx
    Dan la sensación que es más sobre su propio programa para edición de
    python que otra cosa

--- test_1.py
+++ test_1.py
@@ -1,1 +1,16 @@
-print("hello world")
+# primera lección
+# hello word
+
+print("hello world!")
+
+# uso de comentarios
+
+# Uso de asiganción de variables y suma
+x = 3;
+y = x + 3;
+print(y)
+
+#lección 1.2
+
+print('con comillas simples')
+print("comillas dobles")
