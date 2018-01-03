3235bac10f9bd89040eca49e5a77b8ae7d9dc335
illu92 <pablomorpal@gmail.com>
2018-01-03 11:26:36 +0100

    Lección 1.3, asignación de variables

--- test_1.py
+++ test_1.py
@@ -14,3 +14,17 @@ print(y)
 
 print('con comillas simples')
 print("comillas dobles")
+
+# lección 1.2 asignación de variables
+
+# Asignación
+student_name = 'Pablo Morcillo'
+
+#echo de la variable
+print(student_name)
+
+# Reasignacion de variables
+
+print(student_name)
+student_name = "lolerino lopez"
+print(student_name)
