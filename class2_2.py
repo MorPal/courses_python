719d30875714b113d696f8e736b0c6840ad38069
illu92 <pablomorpal@gmail.com>
2018-01-03 12:49:34 +0100

    Simple examples with prints, commas and formatts

---
+++ class2_2.py
@@ -0,0 +1,13 @@
+# Class 2.2
+
+# Class formatting
+
+name = "Collette"
+print("hello " + name + "!")
+
+print("hello",name,"!")
+
+# So, how to print commas? -> with ','
+
+num_error = 0;
+print("Int = ", 14, "with strings causes",num_error, "errors in comma format!")
