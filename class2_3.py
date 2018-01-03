1df3d89d6902088789ac1339eb0b0a52e95d3f4e
illu92 <pablomorpal@gmail.com>
2018-01-03 13:10:25 +0100

    Class 2.3
    Quotes inside strings

--- class2_3.py
+++ class2_3.py
@@ -5,3 +5,14 @@
 print("it's hammer time")
 
 print("He said: 'oh la la'")
+
+# boolean strings
+
+"Python".isalpha()      #true // only letters of the alphabet
+"Python! ".isalpha()    #false
+"3rd".isalnum()         #true digit and alphabetics // alphanumeric
+"3rd ".isalnum()        #false - space
+"save".starswith('s')   #true
+"HEY".itupper()         #true
+"The End".istitle       #true - each word separate
+"The end".istitle       #false
