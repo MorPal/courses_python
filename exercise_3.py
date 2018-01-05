928a8bf361cfeac04dea6dd324f896fdb3f49f4e
illu92 <pablomorpal@gmail.com>
2018-01-05 11:27:56 +0100

    Exercises from the module 3

---
+++ exercise_3.py
@@ -0,0 +1,37 @@
+# Exercise 3
+
+# [ ] define and call a function short_rhyme() that prints a 2 line rhyme
+def short_rhyme(line1, line2):
+    return print(line1 + '\n' + line2)
+
+l1 = input('Rhyme 1: '.lower())
+l2 = input('Rhyme 2: '.lower())
+
+short_rhyme(l1,l2)
+
+print('########' + '\n------' + '\n########')
+
+# [ ] define (def) a simple function: title_it() and call the function
+# - has a string parameter: msg
+# - prints msg in Title Case
+
+def title_it(msg):
+    return print(msg.title())
+
+title_it(l1)
+title_it(l2)
+
+# [ ] get user input with prompt "what is the title?"
+# [ ] call title_it() using input for the string argument
+
+title = input('what is the title?: ')
+title_it(title)
+
+# [ ] define title_it_rtn() which returns a titled string instead of printing
+# [ ] call title_it_rtn() using input for the string argument and print the result
+
+def title_it_rtn(msg):
+    return msg.title()
+
+rtn = title_it_rtn(title)
+print(rtn)
