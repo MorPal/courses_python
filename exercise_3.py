59d73183bd1831b48a635d62f3e9c4931fba4ea1
illu92 <pablomorpal@gmail.com>
2018-01-05 11:30:15 +0100

    Minus error fixeds

--- exercise_3.py
+++ exercise_3.py
@@ -35,3 +35,19 @@ def title_it_rtn(msg):
 
 rtn = title_it_rtn(title)
 print(rtn)
+
+#########
+
+def make_greeting(name, greeting = "Hello"):
+    return (greeting + " " + name + "!")
+    
+def get_name():
+    name_entry = input("enter a name: ")
+    return name_entry
+
+def get_greeting():
+    greeting_entry = input("enter a greeting: ")
+    return greeting_entry
+
+# get name and greeting, send to make_greeting
+print(make_greeting(get_name(), get_greeting()))
