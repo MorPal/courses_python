fca1ed7e420356e9443485937974f326b88caa1c
illu92 <pablomorpal@gmail.com>
2018-01-06 12:38:44 +0100

    Class 2

---
+++ Class2/class2_1.py
@@ -0,0 +1,12 @@
+# Class 2.1
+
+# Inputs
+
+print("Enter your name: ")
+student_name = input()
+
+print('your name is ' + student_name)
+
+# Input prompts
+
+student_note = input("Enter your mark here: ")
