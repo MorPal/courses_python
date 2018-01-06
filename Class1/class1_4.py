0b91dbe7723ae15a073c8b0177e6a6257c9a7ed9
illu92 <pablomorpal@gmail.com>
2018-01-06 12:37:05 +0100

    Class 1

---
+++ Class1/class1_4.py
@@ -0,0 +1,31 @@
+# Class 1.4
+
+# Addition with Strings and Integers
+
+# Simple addition
+x = 19 + 30
+print(x)
+
+y = 3.14 + 5.93
+print(y)
+
+print(3+4)
+
+print("Hello, "+"my name is"+" maramiau")
+
+# Add string
+first_name = "tra"
+second_name = "oper"
+print(first_name + second_name)
+
+num = 3
+add_two = num + 2
+add_four = add_two + add_two
+
+print(add_two)
+print(add_four)
+
+# error
+# 1_student = 'prueba'
+student_1 = 'prueba'
+print(student_1)
