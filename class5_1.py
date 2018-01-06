2298dbaf0be07ea98d925a29aa692c7f833e6686
illu92 <pablomorpal@gmail.com>
2018-01-06 11:07:58 +0100

    Class 5.1
    Conditionals: elif
    Casting

--- class5_1.py
+++ class5_1.py
@@ -42,3 +42,31 @@ weight2 = 170 # an integer
 # add 2 integers
 total_weight = int(weight1) + weight2
 print(total_weight)
+
+
+# [ ] review and run code
+student_age = input('enter student age (integer): ')
+age_next_year = int(student_age) + 1
+print('Next year student will be',age_next_year)
+
+# [ ] review and run code
+# cast to int at input
+student_age = int(input('enter student age (integer): '))
+
+age_in_decade = student_age + 10
+
+print('In a decade the student will be', age_in_decade)
+
+# Exercise
+
+size_num = "8 9 10"
+size = "8" # user input
+print(type(size))
+if size.isdigit() == False:
+    print("Invalid: size should only use digits")
+elif int(size) < 8:
+    print("size is too low")
+elif size in size_num:
+    print("size is recorded")
+else:
+    print("size is too high")
