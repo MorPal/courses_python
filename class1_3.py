2f0a9ebc6f26cffdb197f58c65a2d0d018597ab8
illu92 <pablomorpal@gmail.com>
2018-01-06 12:45:59 +0100

    Deleted files

--- class1_3.py
+++
@@ -1,9 +0,0 @@
-# Class 1.3
-
-# type() indicates the type of the variable between ()
-type(4)             #int
-type(3.14)          #float
-type('Hola mundo')  #string
-
-student = "Pablo"
-type(student)       #string
